541bf0e4429566afc22e7b936ecb8b3239dff7fe
zohaib1020 <zohaib1020@1c4f4870-43d4-1367-4bf8-8c629ebb34a3>
2011-11-01 02:40:23 +0000

    Changed list selections from layout changes to starting new activities.

---
+++ trunk/MetaWatch/src/org/metawatch/manager/Email.java
@@ -0,0 +1,13 @@
+package org.metawatch.manager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Email extends Activity
+{	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.email);
+	}
+}
