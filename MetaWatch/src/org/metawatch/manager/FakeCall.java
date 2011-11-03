8937a59e686dd915dcbf2561f3f3671ce62c5502
zohaib1020 <zohaib1020@1c4f4870-43d4-1367-4bf8-8c629ebb34a3>
2011-11-03 22:53:36 +0000

    Updated Fake Call to work. Fixed bug in Last Text. Testing Ringtone playing.

--- MetaWatch/src/org/metawatch/manager/FakeCall.java
+++ MetaWatch/src/org/metawatch/manager/FakeCall.java
@@ -1,8 +1,17 @@
 package org.metawatch.manager;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.provider.Settings;
 
 public class FakeCall extends Activity
 {	
@@ -10,6 +19,48 @@ public class FakeCall extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fakecall);
-        Toast.makeText(getApplicationContext(), "Something", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), "Something", Toast.LENGTH_SHORT).show();
+        
+        final Button button = (Button) findViewById(R.id.FakeCallSaveButton);
+        final EditText name = (EditText) findViewById(R.id.FakeCallName); 
+        final EditText phone = (EditText) findViewById(R.id.FakeCallPhone); 
+        final SharedPreferences settings = getPreferences(MODE_WORLD_READABLE);
+        
+        name.setText(settings.getString("FakeCallName", "Enter Name Here:"));
+        phone.setText(settings.getString("FakeCallPhone", "000-000-0000"));
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	SharedPreferences.Editor editor = settings.edit();
+            	editor.putString("FakeCallName", name.getText().toString());
+            	editor.putString("FakeCallPhone", phone.getText().toString());
+            	editor.commit();
+            	Toast.makeText(getApplicationContext(), "Saved Data", Toast.LENGTH_SHORT).show();
+            	Toast.makeText(getApplicationContext(), name.getText().toString(), Toast.LENGTH_SHORT).show();
+            	PlayRingtone();
+            }
+        });
+	}
+	
+	public void PlayRingtone()
+	{
+		MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+		
+		if(player == null)
+		{
+			Toast.makeText(getApplicationContext(), "Cannot Play Ringtone", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		try {
+			player.prepare();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		player.start();
 	}
 }
